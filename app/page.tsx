f943d84a5a4891e539a194d5b7e17fd1d021a360
John Sanico <sanicojohn28@gmail.com>
2026-09-10 21:35:03 -0700

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+import Link from "next/link";
+import { createClient } from "./lib/supabase/server";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World!</h1>
-    </div>
-  );
+export default async function DashboardPage (){
+    const supabase = await createClient();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+   
+    return (
+        <div className="flex min-h-screen justify-center items-center bg-black">
+            <div className="w-full max-w-md space-y-8">
+                <div className="text-center">
+                    <h1 className="text-white text-3xl font-semibold">Welcome</h1>
+                    <p className="text-sm text-gray-400 mt-2">
+                        {user ? `Hello, ${user.user_metadata?.name || user.email?.split('@')[0] || "User"}` : `get Started with your account`}
+                    </p>
+                </div>
+                <div className="flex flex-col gap-4">
+                  {user ? (<Link className="text-black bg-white rounded-lg px-6 py-3" href={"/dashboard"}>Go to Dashboard</Link>) : (
+                    <>
+                    <Link className="text-black bg-white rounded-lg px-6 py-3" href={"/login"}>Sign In</Link>
+                    <Link className="text-black bg-white rounded-lg px-6 py-3" href={"/register"}>Sign Up</Link>
+                    </>
+                  )}
+                </div>
+            </div>
+        </div>
+    );
 }
